ddec6fb055a374e9fd53cb7d67d32916943fb8ba
Vinicius Oliveira <87101202+ViOliPro@users.noreply.github.com>
2026-09-15 21:00:19 -0300

    task/AddDurezaCalcicaAnalise

---
+++ PiscinaPerfeita.Api/src/Migrations/20260915235707_AddDurezaCalcicaAnalise.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PiscinaPerfeita.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDurezaCalcicaAnalise : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "durezacalcica",
+                schema: "piscina-perfeita",
+                table: "Analises",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "durezacalcica",
+                schema: "piscina-perfeita",
+                table: "Analises");
+        }
+    }
+}
